5ed81bbb0324f88adb869c18ba022e06dc7d2b64
足羽川永都(あすわがわえいと) <wakeup@8am.jp>
2017-10-07 11:20:10 +0900

    add ebook.js

---
+++ js/ebook.js
@@ -0,0 +1,15 @@
+$(function(){
+  $('article').each(function() {
+    var article = $(this).html();
+    /*
+     ルビを変換します。
+     「｜漢字《かんじ》」を「<ruby>漢字<rt>かんじ</rt></ruby>」に変換します。
+     「｜ひらがな《Hiragana》」も「<ruby>ひらがな<rt>Hiragana</rt></ruby>」になります。
+     */
+    $(this).html(
+      article
+        .replace(/｜?([一-龠]+)《(.+?)》/g, '<ruby>$1<rt>$2</rt></ruby>')
+        .replace(/｜([^《]+)《(.+?)》/g,    '<ruby>$1<rt>$2</rt></ruby>')
+    );
+  });
+});
