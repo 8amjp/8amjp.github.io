464782a9b8b6e345f3b36f977456a9c839f3b845
足羽川永都(あすわがわえいと) <wakeup@8am.jp>
2017-10-09 11:37:08 +0900

    Update ebook.js

--- js/ebook.js
+++ js/ebook.js
@@ -8,8 +8,8 @@ $(function(){
      */
     $(this).html(
       article
-        .replace(/｜?([一-龠]+)《(.+?)》/g, '<ruby>$1<rt>$2</rt></ruby>')
-        .replace(/｜([^《]+)《(.+?)》/g,    '<ruby>$1<rt>$2</rt></ruby>')
+        .replace(/｜([^《]+)《(.+)》/g,    '<ruby>$1<rt>$2</rt></ruby>')
+        .replace(/｜?([一-龠]+)《(.+)》/g, '<ruby>$1<rt>$2</rt></ruby>')
     );
   });
 });
