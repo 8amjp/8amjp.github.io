cd2ef0342da9b9216cce91a7a902c3bf77a4c07f
足羽川永都(あすわがわえいと) <wakeup@8am.jp>
2017-10-11 23:29:30 +0900

    update

--- js/ebook.js
+++ js/ebook.js
@@ -10,7 +10,7 @@ $(function(){
       article
         .replace(/｜《(.+?)》/g,    '《$1》')
         .replace(/｜(.+?)《(.+?)》/g,    '<ruby>$1<rt>$2</rt></ruby>')
-        .replace(/([ヶ一-龠]+?)《(.+?)》/g, '<ruby>$1<rt>$2</rt></ruby>')
+        .replace(/([ヶ一-龠]+)《(.+?)》/g, '<ruby>$1<rt>$2</rt></ruby>')
     );
   });
 });
